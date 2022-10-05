3bdaf340fd292b2ef04815112df734f1de06f385
PJ Fanning <pjfanning@users.noreply.github.com>
2022-10-05 22:34:32 +0100

    Update Main.scala

--- src/main/scala/sizeof/Main.scala
+++ src/main/scala/sizeof/Main.scala
@@ -3,6 +3,9 @@ package sizeof
 import com.madhukaraphatak.sizeof.SizeEstimator
 import org.slf4j.LoggerFactory
 
+import java.util
+import java.util.ArrayList
+
 object Main extends App {
   private val logger = LoggerFactory.getLogger(Main.getClass)
 
@@ -31,17 +34,27 @@ object Main extends App {
     @transient val hash32: Int = 0
   }
 
-//  checkSizesMatch(DummyString(""))
-//  checkSizesMatch(DummyString("a"))
-//  checkSizesMatch(DummyString("ab"))
-//  checkSizesMatch(DummyString("abcdefgh"))
-//  checkSizesMatch(Array.fill(10)(new DummyClass1))
-//  checkSizesMatch(Array.fill(10)(new DummyClass2))
-//  checkSizesMatch(Array.fill(10)(new DummyClass3))
-//  checkSizesMatch(Array(new DummyClass1, new DummyClass2))
+  checkSizesMatch(DummyString(""))
+  checkSizesMatch(DummyString("a"))
+  checkSizesMatch(DummyString("ab"))
+  checkSizesMatch(DummyString("abcdefgh"))
+  checkSizesMatch(Array.fill(10)(new DummyClass1))
+  checkSizesMatch(Array.fill(10)(new DummyClass2))
+  checkSizesMatch(Array.fill(10)(new DummyClass3))
+  checkSizesMatch(Array(new DummyClass1, new DummyClass2))
   checkSizesMatch(Array.fill(1000)(new DummyClass1))
   checkSizesMatch(Array.fill(1000)(new DummyClass2))
   checkSizesMatch(Array.fill(1000)(new DummyClass3))
+
+  val javaList = new util.ArrayList[Long]()
+  for (i <- 0 until 10) {
+    javaList.add(java.lang.Long.valueOf(i))
+  }
+  checkSizesMatch(javaList)
+  for (i <- 0 until 10) {
+    javaList.add(java.lang.Long.valueOf(i))
+  }
+  checkSizesMatch(javaList)
 
   def checkSizesMatch[T](t: T): Unit = {
     val psize = JavaSizeEstimator.estimate(t)
